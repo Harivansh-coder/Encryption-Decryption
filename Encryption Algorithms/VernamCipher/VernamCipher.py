74e907c99d3ad68d86a42bd678eb3fbd30285fa7
Harivansh Thakur <71117889+Harivansh-coder@users.noreply.github.com>
2021-07-31 18:05:47 +0530

    Update VernamCipher.py

--- Encryption Algorithms/VernamCipher/VernamCipher.py
+++ Encryption Algorithms/VernamCipher/VernamCipher.py
@@ -7,27 +7,58 @@ Condition: The key length must match the length of the message.
 
 # Encrypting function
 def encrypt(text, key):
-
-    # checking if the condition meets
-    if len(key) != len(text):
-        return "Both key and text should be of same length"
+    global decrypt_imp
     
-    decrypt_imp = {}
+    key = list(key)
+    decrypt_imp = [] # important for decrypting
     result = ""
-    for i, j in zip(text,key):
-        temp = (ord(i) - 65)+ (ord(j) - 65)
+
+    j = -1
+    for i in text:
+        if j < len(key) - 1:
+            j += 1
+        else:
+            j = 0
+        temp = (ord(i) - 65)+ (ord(key[j]) - 65)
         
         k = 0
         while temp > 25:
             temp -= 26
             k += 1
+        decrypt_imp.append(k)
+    
+        result += chr(temp + 65)
+    return result
+    
+# Decrypting function
+def decrypt(text, key):
+    global decrypt_imp
+    key = list(key)
+
+    result = ""
+
+    j = -1
+    for i in text:
+        if j < len(key) - 1:
+            j += 1
+        else:
+            j = 0   
+
+        k = 0
+
+        temp = (ord(i) - 65)- (ord(key[j]) - 65)
+        while True:
+            if decrypt_imp[k] != 0:
+                temp += 26
+                decrypt_imp[k] -= 1
+            else:
+                k += 1
+                break
 
         
         result += chr(temp + 65)
-        decrypt_imp[result] = k
     return result
     
-
 
 if __name__ == "__main__":
 
@@ -37,13 +68,15 @@ if __name__ == "__main__":
 
     # Performing the encryption 
     encrypted_text = encrypt(plain_text, keyword)
-
+    decrypted_text = decrypt(encrypted_text,keyword)
     print(encrypted_text)
+    print(decrypted_text)
 
 
 """
-#Output
+# Output
     Enter the text: HELLO
     Enter the Key: DGHBC
     KKSMQ
+    HELLO
 """
